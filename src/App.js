bc3c85eea7998af46f843d8a6ead7cf92b80dc0b
Marouf <maroufibrahimi7264@gmail.com>
2023-04-17 11:14:50 -0700

    rendered all component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import React from "react"
+import { Route, Routes } from "react-router-dom"
+
+import "./App.css"
+
+import Navbar from "./components/Navbar/Navbar"
+import Home from "./pages/Home/Home"
+import Search from "./pages/Search/Search"
+import Students from "./pages/Students/Students"
 
 const App = () => {
-  return <div className="App">Template</div>
+  return (
+    <div className="app">
+      <main>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/students" element={<Students />} />
+        </Routes>
+      </main>
+    </div>
+  )
 }
 
 export default App
