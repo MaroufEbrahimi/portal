07d000348210e59485196202a99dec4aa0723a70
Marouf <maroufibrahimi7264@gmail.com>
2023-04-16 22:29:13 -0700

    Home component created

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Build from "../../components/Build/Build"
+
+const Home = () => {
+  return (
+    <div>
+      <Build />
+    </div>
+  )
+}
+
+export default Home
