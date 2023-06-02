84cd2665eda0001bef320ee80183bfac87c28b9e
Marouf <maroufibrahimi7264@gmail.com>
2023-06-01 23:37:31 -0700

    added Navbar menu

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react"
 import { Link, useResolvedPath, useMatch } from "react-router-dom"
+import LOGO from "../../assets/img/logo.png"
 import "./Navbar.css"
 
 const CustomeLinks = ({ to, children, ...props }) => {
@@ -18,18 +19,25 @@ const CustomeLinks = ({ to, children, ...props }) => {
 const Navbar = () => {
   return (
     <div className="navbar">
+      <div className="logo">
+        <img src={LOGO} alt="logo" className="logo_img" />
+      </div>
       <ul className="navbar_menu">
-        <CustomeLinks to="/">
+        <CustomeLinks to="/" title="خانه">
           <i className="bi bi-house-door"></i>
-          <span>Home</span>
+          <span>خانه</span>
         </CustomeLinks>
-        <CustomeLinks to="/search">
+        <CustomeLinks to="/search" title="جستجو">
           <i className="bi bi-search"></i>
-          <span>Search</span>
+          <span>جستجو</span>
         </CustomeLinks>
-        <CustomeLinks to="/students">
+        <CustomeLinks to="/students" title="محصلین">
           <i className="bi bi-people"></i>
-          <span>Students</span>
+          <span>محصلین</span>
+        </CustomeLinks>
+        <CustomeLinks to="/sttings" title="تنظیمات">
+          <i className="bi bi-gear"></i>
+          <span>تنظیمات</span>
         </CustomeLinks>
       </ul>
     </div>
