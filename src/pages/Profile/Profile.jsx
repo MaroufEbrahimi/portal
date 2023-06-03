c6ebf39e516d97d4cead0556908027080cd2ed94
Marouf <maroufibrahimi7264@gmail.com>
2023-06-02 22:41:51 -0700

    added state for switch between tabs

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,9 +1,14 @@
-import React from "react"
-import { Link } from "react-router-dom"
+import React, { useState } from "react"
 import USER from "../../assets/img/user.jpg"
 import "./Profile.css"
 
 const Profile = () => {
+  const [showTab, setShowTab] = useState(1)
+
+  const handleTabs = (index) => {
+    setShowTab(index)
+  }
+
   return (
     <div className="profile">
       <div className="profile_title">
@@ -12,11 +17,67 @@ const Profile = () => {
           <h1>معروف ابراهیمی</h1>
         </div>
       </div>
-      <div className="profile_links">
+      <div className="tab_header">
         <ul>
-          <li><Link to="/">معلومات شخصی</Link></li>
-          <li><Link to="/">معلومات دانشگاه</Link></li>
+          <li
+            className={showTab === 1 ? "active_tab" : ""}
+            onClick={() => handleTabs(1)}
+          >
+            <span>معلومات شخصی</span>
+          </li>
+          <li
+            className={showTab === 2 ? "active_tab" : ""}
+            onClick={() => handleTabs(2)}
+          >
+            <span>معلومات دانشگاه</span>
+          </li>
         </ul>
+      </div>
+
+      <div className={showTab === 1 ? "content active_content" : "content"}>
+        <div className="content_boxes">
+          <div className="content_box">
+            <label>نام</label>
+            <p>معروف</p>
+          </div>
+          <div className="content_box">
+            <label>تخلص</label>
+            <p>ابراهیمی</p>
+          </div>
+          <div className="content_box">
+            <label>نام پدر</label>
+            <p>ابراهیمی</p>
+          </div>
+          <div className="content_box">
+            <label>شماره تماس</label>
+            <p>0799503350</p>
+          </div>
+          <div className="content_box">
+            <label>ایمیل</label>
+            <p>famous@gmail.com</p>
+          </div>
+          <div className="content_box">
+            <label>سال شمولیت</label>
+            <p>1398</p>
+          </div>
+          <div className="content_box">
+            <label>دیپارتمنت</label>
+            <p>سافت ویر</p>
+          </div>
+          <div className="content_box">
+            <label>سال</label>
+            <p>4</p>
+          </div>
+          <div className="content_box">
+            <label>سمستر</label>
+            <p>8</p>
+          </div>
+        </div>
+      </div>
+
+      <div className={showTab === 2 ? "content active_content" : "content"}>
+        <h1>university info</h1>
+        <p>Lorem ipsum dolor sit amet consectetur</p>
       </div>
     </div>
   )
