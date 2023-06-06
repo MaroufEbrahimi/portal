f9580db83f6811c17ef1a296d68651011d285077
Marouf <maroufibrahimi7264@gmail.com>
2023-06-05 23:47:37 -0700

    added redcer on index file

--- src/index.js
+++ src/index.js
@@ -3,13 +3,17 @@ import { createRoot } from "react-dom/client"
 import "./index.css"
 import "./assets/bootstrap-icons/bootstrap-icons.css"
 import { BrowserRouter } from "react-router-dom"
+import { StateProvider } from "./constants/StateProvider"
+import reducer, { initialState } from "./constants/reducer"
 
 import App from "./App"
 
 const container = document.getElementById("root")
 const root = createRoot(container)
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <StateProvider initialState={initialState} reducer={reducer}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StateProvider>
 )
