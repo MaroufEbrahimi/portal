604de7cbaeadf3dccaf5ff7effe90c326ee0e35d
Marouf <maroufibrahimi7264@gmail.com>
2023-06-08 05:31:05 -0700

    using React.lazy for import components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import React from "react"
-import { Route, Routes } from "react-router-dom"
+import React, { Suspense } from "react"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 
 import "./App.css"
+import Loading from "./components/UI/Loading/Loading"
+import Wrapper from "./components/HOC/Wrapper"
 
-import Navbar from "./components/Navbar/Navbar"
-import Home from "./pages/Home/Home"
-import Search from "./pages/Search/Search"
-import Students from "./pages/Students/Students"
+const Navbar = React.lazy(() => import("./components/Navbar/Navbar"))
+const Home = React.lazy(() => import("./pages/Home/Home"))
+const SearchPage = React.lazy(() => import("./pages/SearchPage/SearchPage"))
+const Students = React.lazy(() => import("./pages/Students/Students"))
+const AddStudent = React.lazy(() => import("./pages/AddStudent/AddStudent"))
+const Profile = React.lazy(() => import("./pages/Profile/Profile"))
+const Login = React.lazy(() => import("./pages/Login/Login"))
+const BackToTop = React.lazy(() => import("./components/BackToTop/BackToTop"))
 
 const App = () => {
   return (
     <div className="app">
       <main>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/search" element={<Search />} />
-          <Route path="/students" element={<Students />} />
-        </Routes>
+        <Router>
+          <Suspense fallback={<Loading />}>
+            <Navbar />
+            <Wrapper>
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/addstudent" element={<AddStudent />} />
+                <Route path="/search" element={<SearchPage />} />
+                <Route path="/students" element={<Students />} />
+                <Route path="/profile" element={<Profile />} />
+                <Route path="/login" element={<Login />} />
+              </Routes>
+            </Wrapper>
+            <BackToTop />
+          </Suspense>
+        </Router>
       </main>
     </div>
   )
