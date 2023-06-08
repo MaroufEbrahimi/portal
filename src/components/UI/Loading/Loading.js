3bd0d6a69dc185454b1bd3489b4ab280ee8be578
Marouf <maroufibrahimi7264@gmail.com>
2023-06-08 05:30:34 -0700

    added Snipper for Loading pages

--- src/components/UI/Loading/Loading.js
+++ src/components/UI/Loading/Loading.js
@@ -3,12 +3,8 @@ import "./Loading.css"
 
 const Loading = () => {
   return (
-    <div className="spinner-wrapper">
-      <div className="spinner">
-        <div className="bounce1"></div>
-        <div className="bounce2"></div>
-        <div className="bounce3"></div>
-      </div>
+    <div className="spinner__wrapper">
+      <div className="spinner"></div>
     </div>
   )
 }
