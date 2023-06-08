8547369450f22ab7a36a278716754b9a7f1e1e8f
Marouf <maroufibrahimi7264@gmail.com>
2023-06-08 14:22:04 -0700

    DeletePage created

---
+++ src/pages/DeleteStudent/DeleteStudent.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react"
+import "./DeleteStudent.css"
+import ModalDelete from "../../components/UI/ModalDelete/ModalDelete"
+
+const DeleteStudent = (props) => {
+  const [showModal, setShowModal] = useState(false)
+  const showModalHandler = () => {
+    setShowModal(true)
+  }
+  const modalCloseHandler = () => {
+    setShowModal(false)
+  }
+  return (
+    <div className="delete_student">
+      <div className="delete_lock_buttons">
+        <button className="btn" onClick={showModalHandler}>
+          پاک کردن
+        </button>
+        <button className="btn">قفل کردن</button>
+        <ModalDelete show={showModal} modalClose={modalCloseHandler}>
+          <div className="delete_cancel_modal">
+            <i className="bi bi-exclamation-diamond"></i>
+            <h3>مطمین هستید؟</h3>
+            <p>بصورت دایمی پاک خواهد شد</p>
+            <div className="del_cancel_btn">
+              <button className="btn delete_btn">پاک کردن</button>
+              <button className="btn" onClick={modalCloseHandler}>
+                نخیر
+              </button>
+            </div>
+          </div>
+        </ModalDelete>
+      </div>
+    </div>
+  )
+}
+
+export default DeleteStudent
