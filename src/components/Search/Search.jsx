59a78b2ca02bd8e0d28c339c22ce3b64b31c8d81
Marouf <maroufibrahimi7264@gmail.com>
2023-06-11 15:45:35 -0700

    added context and reducer

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,14 +1,21 @@
 import React, { useState } from "react"
 import { useNavigate } from "react-router-dom"
 import "./Search.css"
+import { useStateValue } from "../../context/StateProvider"
+import { actionTypes } from "../../context/reducer"
 
-const Search = () => {
+const Search = ({ hiddenButton = false }) => {
+  const [{}, dispatch] = useStateValue()
   const [inputSearch, setInputSearch] = useState("")
   const navigate = useNavigate()
 
   const search = (e) => {
     e.preventDefault()
     console.log("click button >>", inputSearch)
+    dispatch({
+      type: actionTypes.SET_SEARCH_TERM,
+      term: inputSearch,
+    })
     navigate("/")
   }
   return (
@@ -22,11 +29,23 @@ const Search = () => {
           onChange={(e) => setInputSearch(e.target.value)}
         />
       </div>
-      <div className="search__button">
-        <button type="submit" onClick={search}>
-          جستجو
-        </button>
-      </div>
+      {!hiddenButton ? (
+        <div className="search__button">
+          <button type="submit" onClick={search}>
+            جستجو
+          </button>
+        </div>
+      ) : (
+        <div className="search__button">
+          <button
+            type="submit"
+            onClick={search}
+            className="search__buttonHidden"
+          >
+            جستجو
+          </button>
+        </div>
+      )}
     </form>
   )
 }
