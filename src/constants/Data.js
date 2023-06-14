a500e6c02da2dbf8a05b9406393d758fb3cdf65f
Marouf <maroufibrahimi7264@gmail.com>
2023-06-13 18:58:41 -0700

    added constant data

---
+++ src/constants/Data.js
@@ -0,0 +1,127 @@
+import sliderImg1 from "../assets/img/slide/slide1.jpg"
+import sliderImg2 from "../assets/img/slide/slide2.jpg"
+import sliderImg3 from "../assets/img/slide/slide3.jpg"
+import sliderImg4 from "../assets/img/slide/slide4.jpg"
+
+import eliteImg1 from "../assets/img/user.jpg"
+
+import profile1 from "../assets/img/user.jpg"
+
+// Main Image Slider
+export const imageSliderData = [
+  {
+    title: "this is the first text for test",
+    desc: "this is the first image slider for test it is not bad for me and another users.",
+    sliderImg: sliderImg1,
+  },
+  {
+    title: "this is the second text for test",
+    desc: "this is the second image slider for test it is not bad for me and another users.",
+    sliderImg: sliderImg2,
+  },
+  {
+    title: "this is the third text for test",
+    desc: "this is the third image slider for test it is not bad for me and another users.",
+    sliderImg: sliderImg3,
+  },
+  {
+    title: "this is the forth text for test",
+    desc: "this is the forth image slider for test it is not bad for me and another users.",
+    sliderImg: sliderImg4,
+  },
+]
+// End of Main Image Slider
+
+// Elite Students
+export const eliteStudents = [
+  {
+    id: 1,
+    text: "رسول الله در حدیث گهربار شفاعت برای ما بیان می دارد که بعد از توقف طولانی بشریت در آن روز بسیار سخت مردم پنجاه هزار سال پای برهنه عریان ایستاده هستند و خورشید بر سرها نزدیک می شود روز بسیار سختی است.",
+    eliteImg: eliteImg1,
+    eliteName: "معروف ابراهیمی",
+    studentsOf: "کامپیوتر ساینس",
+  },
+  {
+    id: 2,
+    text: "رسول الله در حدیث گهربار شفاعت برای ما بیان می دارد که بعد از توقف طولانی بشریت در آن روز بسیار سخت مردم پنجاه هزار سال پای برهنه عریان ایستاده هستند و خورشید بر سرها نزدیک می شود روز بسیار سختی است.",
+    eliteImg: eliteImg1,
+    eliteName: "علی نوری",
+    studentsOf: "کامپیوتر ساینس",
+  },
+  {
+    id: 3,
+    text: "رسول الله در حدیث گهربار شفاعت برای ما بیان می دارد که بعد از توقف طولانی بشریت در آن روز بسیار سخت مردم پنجاه هزار سال پای برهنه عریان ایستاده هستند و خورشید بر سرها نزدیک می شود روز بسیار سختی است.",
+    eliteImg: eliteImg1,
+    eliteName: "نبی عطایی",
+    studentsOf: "کامپیوتر ساینس",
+  },
+]
+// End of Elite Students
+
+// All Students
+export const allStudents = [
+  {
+    profileImg: profile1,
+    studentName: "معروف ابراهیمی",
+    studentEmail: "famous@gmail.com",
+    studentDepartement: "سافت ویر",
+    studentSemester: "8",
+  },
+  {
+    profileImg: profile1,
+    studentName: "معروف ابراهیمی",
+    studentEmail: "famous@gmail.com",
+    studentDepartement: "سافت ویر",
+    studentSemester: "8",
+  },
+  {
+    profileImg: profile1,
+    studentName: "معروف ابراهیمی",
+    studentEmail: "famous@gmail.com",
+    studentDepartement: "سافت ویر",
+    studentSemester: "8",
+  },
+  {
+    profileImg: profile1,
+    studentName: "معروف ابراهیمی",
+    studentEmail: "famous@gmail.com",
+    studentDepartement: "سافت ویر",
+    studentSemester: "8",
+  },
+  {
+    profileImg: profile1,
+    studentName: "معروف ابراهیمی",
+    studentEmail: "famous@gmail.com",
+    studentDepartement: "سافت ویر",
+    studentSemester: "8",
+  },
+  {
+    profileImg: profile1,
+    studentName: "معروف ابراهیمی",
+    studentEmail: "famous@gmail.com",
+    studentDepartement: "سافت ویر",
+    studentSemester: "8",
+  },
+  {
+    profileImg: profile1,
+    studentName: "معروف ابراهیمی",
+    studentEmail: "famous@gmail.com",
+    studentDepartement: "سافت ویر",
+    studentSemester: "8",
+  },
+  {
+    profileImg: profile1,
+    studentName: "معروف ابراهیمی",
+    studentEmail: "famous@gmail.com",
+    studentDepartement: "سافت ویر",
+    studentSemester: "8",
+  },
+  {
+    profileImg: profile1,
+    studentName: "معروف ابراهیمی",
+    studentEmail: "famous@gmail.com",
+    studentDepartement: "سافت ویر",
+    studentSemester: "8",
+  },
+]
+// End of All Students
