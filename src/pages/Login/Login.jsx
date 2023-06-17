532e830f162ef0533e2c4494d8fb854789317cec
Marouf <maroufibrahimi7264@gmail.com>
2023-06-16 17:31:44 -0700

    added authentication on login page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,8 +1,30 @@
-import React from "react"
+import React, { useContext, useState } from "react"
+import { useNavigate } from "react-router-dom"
+import { AuthContext } from "../../context/authContext"
 import "./Login.css"
-import LOGO from "../../assets/img/logo.png"
 
 const Login = () => {
+  const [inputs, setInputs] = useState({
+    email: "",
+    password: "",
+  })
+  const [error, setError] = useState(null)
+  const handleChange = (e) => {
+    setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }))
+  }
+  const { login } = useContext(AuthContext)
+  const navigate = useNavigate()
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    try {
+      await login(inputs)
+      navigate("/")
+    } catch (err) {
+      setError(err.response.data)
+    }
+  }
+
   return (
     <div className="login fade_in">
       <div className="login_container login_details">
@@ -12,16 +34,29 @@ const Login = () => {
         <form action="">
           <div className="form_container">
             <div className="login_box">
-              <input type="text" placeholder="Email" />
+              <input
+                type="text"
+                placeholder="Email"
+                name="email"
+                onChange={handleChange}
+              />
               <i className="bi bi-person-circle"></i>
             </div>
             <div className="login_box">
-              <input type="password" placeholder="Password" />
+              <input
+                type="password"
+                placeholder="Password"
+                name="password"
+                onChange={handleChange}
+              />
               <i className="bi bi-lock-fill"></i>
             </div>
           </div>
+          {error && "ایمیل یا رمز اشتباه است!"}
           <div className="btn_login">
-            <button className="login_button">ورود به سیستم</button>
+            <button className="login_button" onClick={handleSubmit}>
+              ورود به سیستم
+            </button>
           </div>
         </form>
       </div>
