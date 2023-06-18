8e075c7e77b918b28f30d7e7413db64b57f03ca3
Marouf <maroufibrahimi7264@gmail.com>
2023-06-17 23:20:49 -0700

    added new component on App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import React, { Suspense } from "react"
+import React, { Suspense, useContext } from "react"
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import { AuthContext } from "./context/authContext"
 
 import "./App.css"
 import Loading from "./components/UI/Loading/Loading"
 import Wrapper from "./components/HOC/Wrapper"
+import Footer from "./components/Footer/Footer"
+import Header from "./components/Header/Header"
 
 const Navbar = React.lazy(() => import("./components/Navbar/Navbar"))
 const Home = React.lazy(() => import("./pages/Home/Home"))
@@ -12,26 +15,47 @@ const Students = React.lazy(() => import("./pages/Students/Students"))
 const AddStudent = React.lazy(() => import("./pages/AddStudent/AddStudent"))
 const Profile = React.lazy(() => import("./pages/Profile/Profile"))
 const Login = React.lazy(() => import("./pages/Login/Login"))
-const BackToTop = React.lazy(() => import("./components/BackToTop/BackToTop"))
+const BackToTop = React.lazy(() =>
+  import("./components/UI/BackToTop/BackToTop")
+)
+const NewPost = React.lazy(() => import("./pages/NewPost/NewPost"))
+const DeleteStudent = React.lazy(() =>
+  import("./pages/DeleteStudent/DeleteStudent")
+)
+const NotFound = React.lazy(() => import("./pages/NotFound/NotFound"))
 
-const App = () => {
+const App = (props) => {
+  const authContext = useContext(AuthContext)
+
   return (
     <div className="app">
       <main>
         <Router>
           <Suspense fallback={<Loading />}>
+            <Header />
             <Navbar />
             <Wrapper>
               <Routes>
                 <Route path="/" element={<Home />} />
-                <Route path="/addstudent" element={<AddStudent />} />
-                <Route path="/search" element={<SearchPage />} />
-                <Route path="/students" element={<Students />} />
-                <Route path="/profile" element={<Profile />} />
+                {authContext.isAuth ? (
+                  <>
+                    <Route path="/admin/addstudent" element={<AddStudent />} />
+                    <Route path="/search" element={<SearchPage />} />
+                    <Route path="/students" element={<Students />} />
+                    <Route path="/profile" element={<Profile />} />
+                    <Route path="/admin/newpost" element={<NewPost />} />
+                    <Route
+                      path="/admin/deletestudent"
+                      element={<DeleteStudent />}
+                    />
+                  </>
+                ) : null}
                 <Route path="/login" element={<Login />} />
+                <Route path="*" element={<NotFound />} />
               </Routes>
             </Wrapper>
             <BackToTop />
+            <Footer />
           </Suspense>
         </Router>
       </main>
