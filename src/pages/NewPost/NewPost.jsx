e339b1d9c70236b45d86111223ca3350c28e9b18
Marouf <maroufibrahimi7264@gmail.com>
2023-06-20 23:52:30 -0700

    new changing

--- src/pages/NewPost/NewPost.jsx
+++ src/pages/NewPost/NewPost.jsx
@@ -6,7 +6,7 @@ import "./NewPost.css"
 const NewPost = () => {
   const [description, setDescription] = useState("")
   return (
-    <div className="new_post">
+    <div className="new_post fade_in">
       <div className="post_description">
         <div className="editorContainer">
           <ReactQuill
@@ -36,35 +36,44 @@ const NewPost = () => {
         </div>
       </div>
 
-      <div className="post_boxes">
-        <div className="post_box">
-          <select id="type">
-            <option>کدام سمستر</option>
-            <option>1</option>
-            <option>2</option>
-            <option>3</option>
-            <option>4</option>
-            <option>5</option>
-            <option>6</option>
-            <option>7</option>
-            <option>8</option>
-          </select>
-        </div>
-        <div className="post_box">
-          <select id="type">
-            <option>کدام دیپارتمنت</option>
-            <option>سافت ویر</option>
-            <option>دیتابیس</option>
-            <option>نتورک</option>
-          </select>
-        </div>
-        <div className="post_box">
-          <select id="type">
-            <option>کدام پوهنحی</option>
-            <option>کامپیوتر ساینس</option>
-            <option>حقوق</option>
-            <option>ستوماتالوژی</option>
-          </select>
+      <div className="share_post_on">
+        <h3>اشتراک گذاری در کجا</h3>
+        <div className="post_boxes">
+          <div className="post_box">
+            <select id="type">
+              <option>سمستر</option>
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
+              <option>4</option>
+              <option>5</option>
+              <option>6</option>
+              <option>7</option>
+              <option>8</option>
+            </select>
+          </div>
+          <div className="post_box">
+            <select id="type">
+              <option>دیپارتمنت</option>
+              <option>سافت ویر</option>
+              <option>دیتابیس</option>
+              <option>نتورک</option>
+            </select>
+          </div>
+          <div className="post_box">
+            <select id="type">
+              <option>پوهنحی</option>
+              <option>کامپیوتر ساینس</option>
+              <option>حقوق</option>
+              <option>ستوماتالوژی</option>
+            </select>
+          </div>
+          <div className="post_box">
+            <select id="type">
+              <option>صفحه محصل</option>
+              <option>صفحه اصلی</option>
+            </select>
+          </div>
         </div>
       </div>
     </div>
--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -2,6 +2,8 @@ import React, { useState } from "react"
 import USER from "../../assets/img/user.jpg"
 import "./Profile.css"
 
+import Post from "../../components/Post/Post"
+
 const Profile = () => {
   const [showTab, setShowTab] = useState(1)
 
@@ -10,7 +12,7 @@ const Profile = () => {
   }
 
   return (
-    <div className="profile">
+    <div className="profile fade_in">
       <div className="profile_title">
         <div className="user_profile_img">
           <img src={USER} alt="user img" />
@@ -34,50 +36,53 @@ const Profile = () => {
         </ul>
       </div>
 
-      <div className={showTab === 1 ? "content active_content" : "content"}>
-        <div className="content_boxes">
-          <div className="content_box">
-            <label>نام</label>
-            <p>معروف</p>
-          </div>
-          <div className="content_box">
-            <label>تخلص</label>
-            <p>ابراهیمی</p>
-          </div>
-          <div className="content_box">
-            <label>نام پدر</label>
-            <p>ابراهیمی</p>
-          </div>
-          <div className="content_box">
-            <label>شماره تماس</label>
-            <p>0799503350</p>
-          </div>
-          <div className="content_box">
-            <label>ایمیل</label>
-            <p>famous@gmail.com</p>
-          </div>
-          <div className="content_box">
-            <label>سال شمولیت</label>
-            <p>1398</p>
-          </div>
-          <div className="content_box">
-            <label>دیپارتمنت</label>
-            <p>سافت ویر</p>
-          </div>
-          <div className="content_box">
-            <label>سال</label>
-            <p>4</p>
-          </div>
-          <div className="content_box">
-            <label>سمستر</label>
-            <p>8</p>
+      <div className="content_of_profile">
+        <div className={showTab === 1 ? "content active_content" : "content"}>
+          <div className="content_boxes">
+            <div className="content_box">
+              <label>نام</label>
+              <p>معروف</p>
+            </div>
+            <div className="content_box">
+              <label>تخلص</label>
+              <p>ابراهیمی</p>
+            </div>
+            <div className="content_box">
+              <label>نام پدر</label>
+              <p>ابراهیمی</p>
+            </div>
+            <div className="content_box">
+              <label>شماره تماس</label>
+              <p>0799503350</p>
+            </div>
+            <div className="content_box">
+              <label>ایمیل</label>
+              <p>famous@gmail.com</p>
+            </div>
+            <div className="content_box">
+              <label>سال شمولیت</label>
+              <p>1398</p>
+            </div>
+            <div className="content_box">
+              <label>دیپارتمنت</label>
+              <p>سافت ویر</p>
+            </div>
+            <div className="content_box">
+              <label>سال</label>
+              <p>4</p>
+            </div>
+            <div className="content_box">
+              <label>سمستر</label>
+              <p>8</p>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div className={showTab === 2 ? "content active_content" : "content"}>
-        <h1>university info</h1>
-        <p>Lorem ipsum dolor sit amet consectetur</p>
+        <div className={showTab === 2 ? "content active_content" : "content"}>
+          <Post />
+          <Post />
+          <Post />
+        </div>
       </div>
     </div>
   )
