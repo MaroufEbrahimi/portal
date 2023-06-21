56dd8df32d308a21943c12a7ba4c356bef373eff
Marouf <maroufibrahimi7264@gmail.com>
2023-06-20 23:52:59 -0700

    student page created

--- src/pages/Students/Students.js
+++ src/pages/Students/Students.js
@@ -1,10 +1,17 @@
 import React from "react"
 import "./Students.css"
+import Student from "../../components/Student/Student"
+import Search from "../../components/Search/Search"
 
 const Students = () => {
   return (
-    <div>
-      <h2>Students</h2>
+    <div className="students_page fade_in">
+      <Search hiddenButton />
+
+      {/* All Students Here */}
+      <div className="all__students">
+        <Student />
+      </div>
     </div>
   )
 }
