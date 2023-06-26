74c8982a0fc21d11a781b0c27a9b025753a1f90a
Marouf <maroufibrahimi7264@gmail.com>
2023-06-26 08:53:40 -0700

    using createBrowserRouter for switch between pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React, { Suspense, useContext, useState } from "react"
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 import { AuthContext } from "./context/authContext"
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet,
+  Navigate,
+} from "react-router-dom"
 
 import "./App.css"
+
 import Loading from "./components/UI/Loading/Loading"
 import Wrapper from "./components/HOC/Wrapper"
 import Footer from "./components/Footer/Footer"
 import Header from "./components/Header/Header"
+import BackToTop from "./components/UI/BackToTop/BackToTop"
 
 const Navbar = React.lazy(() => import("./components/Navbar/Navbar"))
 const Home = React.lazy(() => import("./pages/Home/Home"))
@@ -15,9 +22,6 @@ const Students = React.lazy(() => import("./pages/Students/Students"))
 const AddStudent = React.lazy(() => import("./pages/AddStudent/AddStudent"))
 const Profile = React.lazy(() => import("./pages/Profile/Profile"))
 const Login = React.lazy(() => import("./pages/Login/Login"))
-const BackToTop = React.lazy(() =>
-  import("./components/UI/BackToTop/BackToTop")
-)
 const NewPost = React.lazy(() => import("./pages/NewPost/NewPost"))
 const DeleteStudent = React.lazy(() =>
   import("./pages/DeleteStudent/DeleteStudent")
@@ -28,39 +32,73 @@ const App = (props) => {
   const authContext = useContext(AuthContext)
   const [activeNav, setActiveNav] = useState(false)
   const navActiveHandler = () => setActiveNav(!activeNav)
-  // {`navbar ${activeNav && "active_nav_right"}`}
-  return (
-    <div className="app">
-      <main className={`main ${activeNav && "main_active_nav"}`}>
-        <Router>
+
+  const Layout = () => {
+    return (
+      <div className="app">
+        <main className={`main ${activeNav && "main_active_nav"}`}>
           <Suspense fallback={<Loading />}>
             <Header />
             <Navbar activeNav={activeNav} navActiveHandler={navActiveHandler} />
             <Wrapper>
-              <Routes>
-                <Route path="/" element={<Home />} />
-                {authContext.isAuth ? (
-                  <>
-                    <Route path="/admin/addstudent" element={<AddStudent />} />
-                    <Route path="/search" element={<SearchPage />} />
-                    <Route path="/students" element={<Students />} />
-                    <Route path="/profile" element={<Profile />} />
-                    <Route path="/admin/newpost" element={<NewPost />} />
-                    <Route
-                      path="/admin/deletestudent"
-                      element={<DeleteStudent />}
-                    />
-                  </>
-                ) : null}
-                <Route path="/login" element={<Login />} />
-                <Route path="*" element={<NotFound />} />
-              </Routes>
+              <Outlet />
             </Wrapper>
-            <BackToTop />
             <Footer />
+            <BackToTop />
           </Suspense>
-        </Router>
-      </main>
+        </main>
+      </div>
+    )
+  }
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/admin/addstudent",
+          element: <AddStudent />,
+        },
+        {
+          path: "/search",
+          element: <SearchPage />,
+        },
+        {
+          path: "/students",
+          element: <Students />,
+        },
+        {
+          path: "/profile",
+          element: <Profile />,
+        },
+        {
+          path: "/admin/newpost",
+          element: <NewPost />,
+        },
+        {
+          path: "/admin/deletestudent",
+          element: <DeleteStudent />,
+        },
+      ],
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "*",
+      element: <NotFound />,
+    },
+  ])
+
+  return (
+    <div>
+      <RouterProvider router={router} />
     </div>
   )
 }
