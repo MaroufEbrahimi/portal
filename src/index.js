40109571c0150b8f22e060bb26e1e9da5324f9b2
Marouf <maroufibrahimi7264@gmail.com>
2023-06-29 12:10:37 -0700

    added some context

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import { createRoot } from "react-dom/client"
 import "./index.css"
 import "./assets/css/bootstrap-icons.css"
 import { StateProvider } from "./context/StateProvider"
+import AuthContextProvider from "./context/authContext"
+import { DarkModeContextProvider } from "./context/darkMode"
 import reducer, { initialState } from "./context/reducer"
 
 import App from "./App"
@@ -10,7 +12,11 @@ import App from "./App"
 const container = document.getElementById("root")
 const root = createRoot(container)
 root.render(
-  <StateProvider initialState={initialState} reducer={reducer}>
-    <App />
-  </StateProvider>
+  <DarkModeContextProvider>
+    <AuthContextProvider>
+      <StateProvider initialState={initialState} reducer={reducer}>
+        <App />
+      </StateProvider>
+    </AuthContextProvider>
+  </DarkModeContextProvider>
 )
