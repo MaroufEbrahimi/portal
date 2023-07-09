0465a322d3563c68eba93c0b05677b6c9f94b70d
Marouf <maroufibrahimi7264@gmail.com>
2023-07-09 10:38:38 -0700

    create a tab header for filtering

--- src/pages/PostManagement/PostManagement.jsx
+++ src/pages/PostManagement/PostManagement.jsx
@@ -3,10 +3,42 @@ import "./PostManagement.css"
 import { PostManagementTabHeader } from "../../constants/Data"
 import Post from "../../components/Post/Post"
 import Search from "../../components/Search/Search"
+import { useStateValue } from "../../context/StateProvider"
 
 const PostManagement = () => {
-  const [showTab, setShowTab] = useState(1)
-  const handleTabs = (index) => setShowTab(index)
+  const [{ authentication }, dispatch] = useStateValue()
+  const [description, setDescription] = useState("")
+  const [semester, setsemester] = useState("")
+  const [department, setdepartment] = useState("")
+  const [feildOfStudy, setfeildOfStudy] = useState("")
+  const [isPublic, setisPublic] = useState("")
+  const sendInfo = () => {
+    const body = {
+      fieldOfStudy: feildOfStudy,
+      department: department,
+      message: description,
+      authorId: 1,
+      semester: semester,
+      isPublic: isPublic == "صفحه اصلی" ? true : false,
+    }
+    fetch("http://localhost:1000/api/v1/posts", {
+      method: "POST",
+      headers: {
+        Auhtorization: "Bearer " + authentication?.token,
+      },
+      body: JSON.stringify(body),
+    })
+      .then((res) => {
+        if (res.ok) {
+          return res.json()
+        } else {
+          throw new Error(res.statusText)
+        }
+      })
+      .then((data) => {
+        console.log(data)
+      })
+  }
 
   return (
     <div className="posts_management">
@@ -26,63 +58,61 @@ const PostManagement = () => {
       </div>
 
       <div className="posts_management_tabHeader">
-        {PostManagementTabHeader.map((item) => (
-          <ul>
-            <li
-              className={showTab === item.counter ? "active_tab" : ""}
-              onClick={() => handleTabs(item.counter)}
+        <div className="posts_management_boxes">
+          <div className="post_mana_box">
+            <select
+              id="type"
+              value={semester}
+              onChange={(e) => setsemester(e.target.value)}
             >
-              <span>{item.text}</span>
-            </li>
-          </ul>
-        ))}
+              <option>سمستر</option>
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
+              <option>4</option>
+              <option>5</option>
+              <option>6</option>
+              <option>7</option>
+              <option>8</option>
+            </select>
+          </div>
+          <div className="post_mana_box">
+            <select
+              id="type"
+              value={feildOfStudy}
+              onChange={(e) => setfeildOfStudy(e.target.value)}
+            >
+              <option disabled>پوهنحی</option>
+              <option>کامپیوتر ساینس</option>
+              <option>حقوق</option>
+              <option>ستوماتالوژی</option>
+            </select>
+          </div>
+          <div className="post_mana_box">
+            <select
+              id="type"
+              value={department}
+              onChange={(e) => setdepartment(e.target.value)}
+            >
+              <option disabled>دیپارتمنت</option>
+              <option>سافت ویر</option>
+              <option>دیتابیس</option>
+              <option>نتورک</option>
+            </select>
+          </div>
+        </div>
+        <div className="posts_management_filter_btn">
+          <button className="btn">فیلتر</button>
+        </div>
       </div>
 
       <div className="content_of_PostManagement">
-        <div className={showTab === 1 ? "content active_content" : "content"}>
+        <div className="content_of_posts_details">
           <Post />
           <Post />
           <Post />
           <Post />
           <Post />
-          <Post />
-        </div>
-
-        <div className={showTab === 2 ? "content active_content" : "content"}>
-          <Post />
-          <Post />
-          <Post />
-          <Post />
-          <Post />
-          <Post />
-          <Post />
-        </div>
-
-        <div className={showTab === 3 ? "content active_content" : "content"}>
-          <Post />
-          <Post />
-          <Post />
-          <Post />
-          <Post />
-        </div>
-
-        <div className={showTab === 4 ? "content active_content" : "content"}>
-          <Post />
-        </div>
-
-        <div className={showTab === 5 ? "content active_content" : "content"}>
-          <Post />
-        </div>
-
-        <div className={showTab === 6 ? "content active_content" : "content"}>
-          <Post />
-        </div>
-
-        <div className={showTab === 7 ? "content active_content" : "content"}>
-          <Post />
-        </div>
-
-        <div className={showTab === 8 ? "content active_content" : "content"}>
           <Post />
         </div>
       </div>
