8e6dc28d2154e719d60090dda24d7837bf760b50
Marouf <maroufibrahimi7264@gmail.com>
2023-07-10 07:42:55 -0700

    added new file for Introduction of Dean of Faculty

--- src/components/FacultiesSections/IntroOfDeanOfFaculty.jsx
+++ src/components/FacultiesSections/IntroOfDeanOfFaculty.jsx
@@ -1,11 +1,60 @@
-import React from 'react'
+import React from "react"
 import "./FacultiesSections.css"
 
-// معرفی ریس پوهنحی
-const IntroOfDeanOfFaculty = () => {
-  return <div className="intro_of_dean_of_faculty">
-    <h1>این رییس است</h1>
-  </div>
+// معرفی رییس پوهنحی
+const IntroOfDeanOfFaculty = ({
+  deanImg,
+  deanName,
+  FieldofStudy,
+  deanDegree,
+  deanDuty,
+  deanCallNum,
+  deanEmail,
+  deanTxt1,
+  deanTxt2,
+}) => {
+  return (
+    <div className="intro_of_dean_of_faculty">
+      <div className="dean_of_faculty_info">
+        <div className="dean_of_img">
+          <img className="main_img" src={deanImg} alt="Dean image" />
+        </div>
+        <div className="dean_of_faculty_details">
+          <ul>
+            <li>
+              <strong>نام کامل</strong>
+              <span>{deanName}</span>
+            </li>
+            <li>
+              <strong>رشته تحصیلی</strong>
+              <span>{FieldofStudy}</span>
+            </li>
+            <li>
+              <strong>مدرک تحصیلی</strong>
+              <span>{deanDegree}</span>
+            </li>
+            <li>
+              <strong>سمت</strong>
+              <span>{deanDuty}</span>
+            </li>
+            <li>
+              <strong>شماره تماس</strong>
+              <span>{deanCallNum}</span>
+            </li>
+            <li>
+              <strong>ایمیل</strong>
+              <span>{deanEmail}</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div className="dean_of_faculty_text">
+        <p>{deanTxt1}</p>
+        <p>{deanTxt2}</p>
+      </div>
+    </div>
+  )
 }
 
 export default IntroOfDeanOfFaculty
