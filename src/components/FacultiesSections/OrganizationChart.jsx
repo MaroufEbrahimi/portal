e4f602816562c7f636e153b42600888dc2870b13
Marouf <maroufibrahimi7264@gmail.com>
2023-07-10 10:58:42 -0700

    added organization chart

--- src/components/FacultiesSections/OrganizationChart.jsx
+++ src/components/FacultiesSections/OrganizationChart.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React from "react"
 import "./FacultiesSections.css"
 
 // چارت تشکیلاتی پوهنحی
-const OrganizationChart = () => {
+const OrganizationChart = ({ chartImg }) => {
   return (
-    <div>OrganizationChart</div>
+    <div className="organizatioin_chart">
+      <div className="faculties_sections_title">
+        <h2>چارت تشکیلاتی</h2>
+      </div>
+      <div className="organization_chart_img">
+        <img src={chartImg} alt="char img" />
+      </div>
+    </div>
   )
 }
 
