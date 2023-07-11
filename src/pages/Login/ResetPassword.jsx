9f9c907be8366670ef66172bfd3dc4354cf829e9
Marouf <maroufibrahimi7264@gmail.com>
2023-07-10 22:17:18 -0700

    new page for change emailc or password

---
+++ src/pages/Login/ResetPassword.jsx
@@ -0,0 +1,30 @@
+import React from "react"
+import "./Login.css"
+import "./ResetPassword.css"
+
+const ResetPassword = () => {
+  return (
+    <div className="reset_password box_shadow">
+      <div className="reset_password_title">
+        <h2>ایمیل و رمز جدید تان را وارد نماید.</h2>
+      </div>
+      <form action="">
+        <div className="form_container reset_password_form">
+          <div className="login_box">
+            <input type="text" placeholder="ایمیل جدید" name="resetemail" />
+            <i className="bi bi-person-circle"></i>
+          </div>
+          <div className="login_box">
+            <input type="password" placeholder="رمز جدید" name="password" />
+            <i className="bi bi-lock-fill"></i>
+          </div>
+        </div>
+        <div className="reset_button">
+          <button className="btn">ارسال</button>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default ResetPassword
