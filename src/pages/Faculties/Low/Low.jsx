41c519e60b347918205b1ad4020be53c6904db7c
Marouf <maroufibrahimi7264@gmail.com>
2023-07-13 15:45:33 -0700

    added departement on Low faculty

--- src/pages/Faculties/Low/Low.jsx
+++ src/pages/Faculties/Low/Low.jsx
@@ -1,10 +1,21 @@
-import React from "react"
+import React, { useState } from "react"
 import "./Low.css"
 import IntroductionOfFaculty from "../../../components/FacultiesSections/IntroductionOfFaculty"
 import IntroOfDeanOfFaculty from "../../../components/FacultiesSections/IntroOfDeanOfFaculty"
 import Goals from "../../../components/FacultiesSections/Goals"
-import { goalsOf_LOW } from "../../../constants/Data"
-import { valuesOf_LOW } from "../../../constants/Data"
+import {
+  goalsOf_LOW,
+  low_reference1,
+  low_reference2,
+  low_reference3,
+  low_reference4,
+  low_reference5,
+  low_reference6,
+  low_reference7,
+  low_reference8,
+  semester_tabHeader,
+  valuesOf_LOW,
+} from "../../../constants/Data"
 import ViewPoint from "../../../components/FacultiesSections/ViewPoint"
 import OrganizationChart from "../../../components/FacultiesSections/OrganizationChart"
 
@@ -12,6 +23,12 @@ import deanImg from "../../../assets/img/deans/عبدالمتین-منیب-768x9
 import chartImg from "../../../assets/img/organization_charts/چارت-تشکیلاتی-پوهنحی-حقوق-1024x853.jpg"
 
 const Low = () => {
+  const [showTabDep, setShowTabDep] = useState(1)
+  const [showTabSemester, setShowTabSemester] = useState(1)
+
+  const handleTabsDep = (index) => setShowTabDep(index)
+  const handleTabsSemester = (index) => setShowTabSemester(index)
+
   return (
     <div className="low">
       {/* introduction of faculty */}
@@ -32,6 +49,509 @@ const Low = () => {
         deanTxt1="حقوق و علوم سیاسی یکی از کهن ترین رشته های علوم انسانی است که در ادوار مختلف با تناسب نیازهای جامعه بشری رشد و توسعه یافته است. این رشته که بازتاب دهنده عدالت و اجرای حق های بشری می باشد، تأمین کننده نیازهای فردی و جمعی نیز بوده و همزمان با پیشرفت های علمی و فنی تکامل یافته، تا جائیکه هیچ حرکت در جامعه بشری وجود ندارد مگر اینکه تحت امر قواعد حقوق قرار داشته و یا باید مطابق با قواعد و مقررات این رشته علمی قرار گیرد. افغانستان نیز از این قاعده کلی و امری استثناء نیست. از همین رو، از زمان های دور مردم این مرز وبوم با مسائل و موضوعات حقوقی آشنایی داشته و در نظام های مختلف بگونه های متفاوت از آن استفاده شده است. همچنین رشد و توسعه نظام حقوقی کشور نشان از آن دارد که ضرورت جامعه به حفظ و پایداری قواعد حقوقی، تبیین و توضیح آنها امر الزامی بوده است. بنابر این، این ضرورت همچنان در صدر نیازهای هر جامعه بشری به ویژه در کشور عزیزمان افغانستان احساس مبرم پنداشته می شود. از این رو، ریاست دانشکده حقوق و علوم سیاسی مؤسسه تحصیلات عالی هریوا بر خود لازم می داند تا هرچه بیشتر و بهتر در زمینه توسعه و رشد مسائل حقوقی، در قالب تدریس و تحقیق و آن هم مبتنی بر اجرای عدالت همت گماشته و زمینه جلب و جذب بیشتر دانشجویان این رشته را مهیا سازد."
         deanTxt2="و پس از تهیه موارد لازم انتظار دارد کادرهای علمی و فنی را به جامعه تقدیم نماید که توانایی علمی و تخصصی ایشان باعث رشد فکری و علمی و در عین حال سبب حل مشکلات ناشی از کژ فهمی های حقوقی گردد. همچنین ریاست دانشکده حقوق و علوم سیاسی مؤسسه تحصیلات عالی هریوا بر خود می بالد که به عنوان یکی از دانشکده های افتخار آفرین این مؤسسه، میزبان جوانان علم جو و حقیقت خواهی قرار دارد که در آینده حقوق دانان و اندیشمندان این سرزمین اند. سرانجام اینکه دانشکده حقوق و علوم سیاسی همچنان عَلم دار هدایت و تأمین کننده حق ها و حقوق جامعه بشری است و این مسئولیت بزرگ بر عهده این دانشکده قرار داشته، لذا ریاست این دانشکده با درک این مطلب متعهد به تأمین و اجرای کلیه این ضرورت های مبرم جامعه بشری می باشد."
       />
+
+      {/* این بخش شامل تمامی دیپارتمنت های پوهنحی مربوطه می شود */}
+      <div className="departements">
+        <div className="faculties_sections_title">
+          <h2>مفردات درسی</h2>
+        </div>
+
+        <div className="view_point_tabHeader faculties_tabHeader">
+          <ul>
+            <li
+              className={showTabDep === 1 ? "active_tab" : ""}
+              onClick={() => handleTabsDep(1)}
+            >
+              <span>قضایی - څارنوالی</span>
+            </li>
+            <li
+              className={showTabDep === 2 ? "active_tab" : ""}
+              onClick={() => handleTabsDep(2)}
+            >
+              <span>اداری - دیپلماسی</span>
+            </li>
+          </ul>
+        </div>
+
+        {/* semesters */}
+        <div
+          className={
+            showTabDep === 1
+              ? "low_tabHeaderDep active_content box_shadow"
+              : "low_tabHeaderDep"
+          }
+        >
+          <div className="view_point_tabHeader faculties_tabHeader semester_refs">
+            {semester_tabHeader.map((item) => (
+              <ul>
+                <li
+                  className={
+                    showTabSemester === item.counter ? "active_tab" : ""
+                  }
+                  onClick={() => handleTabsSemester(item.counter)}
+                >
+                  <span>{item.semester}</span>
+                </li>
+              </ul>
+            ))}
+          </div>
+
+          <div
+            className={
+              showTabSemester === 1
+                ? "low_tabHeader active_content box_shadow"
+                : "low_tabHeader"
+            }
+          >
+            <div className="low_ref_title">
+              <ul>
+                <li>ماژول</li>
+                <li>تعداد کردیت</li>
+              </ul>
+            </div>
+            <div className="low_ref">
+              {low_reference1.map((item) => (
+                <ul>
+                  <li>
+                    <span>{item.moudle}</span>
+                  </li>
+                  <li>
+                    <span>{item.credit}</span>
+                  </li>
+                </ul>
+              ))}
+            </div>
+          </div>
+
+          <div
+            className={
+              showTabSemester === 2
+                ? "low_tabHeader active_content box_shadow"
+                : "low_tabHeader"
+            }
+          >
+            <div className="low_ref_title">
+              <ul>
+                <li>ماژول</li>
+                <li>تعداد کردیت</li>
+              </ul>
+            </div>
+            <div className="low_ref">
+              {low_reference2.map((item) => (
+                <ul>
+                  <li>
+                    <span>{item.moudle}</span>
+                  </li>
+                  <li>
+                    <span>{item.credit}</span>
+                  </li>
+                </ul>
+              ))}
+            </div>
+          </div>
+
+          <div
+            className={
+              showTabSemester === 3
+                ? "low_tabHeader active_content box_shadow"
+                : "low_tabHeader"
+            }
+          >
+            <div className="low_ref_title">
+              <ul>
+                <li>ماژول</li>
+                <li>تعداد کردیت</li>
+              </ul>
+            </div>
+            <div className="low_ref">
+              {low_reference3.map((item) => (
+                <ul>
+                  <li>
+                    <span>{item.moudle}</span>
+                  </li>
+                  <li>
+                    <span>{item.credit}</span>
+                  </li>
+                </ul>
+              ))}
+            </div>
+          </div>
+
+          <div
+            className={
+              showTabSemester === 4
+                ? "low_tabHeader active_content box_shadow"
+                : "low_tabHeader"
+            }
+          >
+            <div className="low_ref_title">
+              <ul>
+                <li>ماژول</li>
+                <li>تعداد کردیت</li>
+              </ul>
+            </div>
+            <div className="low_ref">
+              {low_reference4.map((item) => (
+                <ul>
+                  <li>
+                    <span>{item.moudle}</span>
+                  </li>
+                  <li>
+                    <span>{item.credit}</span>
+                  </li>
+                </ul>
+              ))}
+            </div>
+          </div>
+
+          <div
+            className={
+              showTabSemester === 5
+                ? "low_tabHeader active_content box_shadow"
+                : "low_tabHeader"
+            }
+          >
+            <div className="low_ref_title">
+              <ul>
+                <li>ماژول</li>
+                <li>تعداد کردیت</li>
+              </ul>
+            </div>
+            <div className="low_ref">
+              {low_reference5.map((item) => (
+                <ul>
+                  <li>
+                    <span>{item.moudle}</span>
+                  </li>
+                  <li>
+                    <span>{item.credit}</span>
+                  </li>
+                </ul>
+              ))}
+            </div>
+          </div>
+
+          <div
+            className={
+              showTabSemester === 6
+                ? "low_tabHeader active_content box_shadow"
+                : "low_tabHeader"
+            }
+          >
+            <div className="low_ref_title">
+              <ul>
+                <li>ماژول</li>
+                <li>تعداد کردیت</li>
+              </ul>
+            </div>
+            <div className="low_ref">
+              {low_reference6.map((item) => (
+                <ul>
+                  <li>
+                    <span>{item.moudle}</span>
+                  </li>
+                  <li>
+                    <span>{item.credit}</span>
+                  </li>
+                </ul>
+              ))}
+            </div>
+          </div>
+
+          <div
+            className={
+              showTabSemester === 7
+                ? "low_tabHeader active_content box_shadow"
+                : "low_tabHeader"
+            }
+          >
+            <div className="low_ref_title">
+              <ul>
+                <li>ماژول</li>
+                <li>تعداد کردیت</li>
+              </ul>
+            </div>
+            <div className="low_ref">
+              {low_reference7.map((item) => (
+                <ul>
+                  <li>
+                    <span>{item.moudle}</span>
+                  </li>
+                  <li>
+                    <span>{item.credit}</span>
+                  </li>
+                </ul>
+              ))}
+            </div>
+          </div>
+
+          <div
+            className={
+              showTabSemester === 8
+                ? "low_tabHeader active_content box_shadow"
+                : "low_tabHeader"
+            }
+          >
+            <div className="low_ref_title">
+              <ul>
+                <li>ماژول</li>
+                <li>تعداد کردیت</li>
+              </ul>
+            </div>
+            <div className="low_ref">
+              {low_reference8.map((item) => (
+                <ul>
+                  <li>
+                    <span>{item.moudle}</span>
+                  </li>
+                  <li>
+                    <span>{item.credit}</span>
+                  </li>
+                </ul>
+              ))}
+            </div>
+          </div>
+        </div>
+
+        <div
+          className={
+            showTabDep === 2
+              ? "low_tabHeaderDep active_content box_shadow"
+              : "low_tabHeaderDep"
+          }
+        >
+          <div className="view_point_tabHeader faculties_tabHeader semester_refs">
+            {semester_tabHeader.map((item) => (
+              <ul>
+                <li
+                  className={
+                    showTabSemester === item.counter ? "active_tab" : ""
+                  }
+                  onClick={() => handleTabsSemester(item.counter)}
+                >
+                  <span>{item.semester}</span>
+                </li>
+              </ul>
+            ))}
+          </div>
+
+          <div
+            className={
+              showTabSemester === 1
+                ? "low_tabHeader active_content box_shadow"
+                : "low_tabHeader"
+            }
+          >
+            <div className="low_ref_title">
+              <ul>
+                <li>ماژول</li>
+                <li>تعداد کردیت</li>
+              </ul>
+            </div>
+            <div className="low_ref">
+              {low_reference1.map((item) => (
+                <ul>
+                  <li>
+                    <span>{item.moudle}</span>
+                  </li>
+                  <li>
+                    <span>{item.credit}</span>
+                  </li>
+                </ul>
+              ))}
+            </div>
+          </div>
+
+          <div
+            className={
+              showTabSemester === 2
+                ? "low_tabHeader active_content box_shadow"
+                : "low_tabHeader"
+            }
+          >
+            <div className="low_ref_title">
+              <ul>
+                <li>ماژول</li>
+                <li>تعداد کردیت</li>
+              </ul>
+            </div>
+            <div className="low_ref">
+              {low_reference2.map((item) => (
+                <ul>
+                  <li>
+                    <span>{item.moudle}</span>
+                  </li>
+                  <li>
+                    <span>{item.credit}</span>
+                  </li>
+                </ul>
+              ))}
+            </div>
+          </div>
+
+          <div
+            className={
+              showTabSemester === 3
+                ? "low_tabHeader active_content box_shadow"
+                : "low_tabHeader"
+            }
+          >
+            <div className="low_ref_title">
+              <ul>
+                <li>ماژول</li>
+                <li>تعداد کردیت</li>
+              </ul>
+            </div>
+            <div className="low_ref">
+              {low_reference3.map((item) => (
+                <ul>
+                  <li>
+                    <span>{item.moudle}</span>
+                  </li>
+                  <li>
+                    <span>{item.credit}</span>
+                  </li>
+                </ul>
+              ))}
+            </div>
+          </div>
+
+          <div
+            className={
+              showTabSemester === 4
+                ? "low_tabHeader active_content box_shadow"
+                : "low_tabHeader"
+            }
+          >
+            <div className="low_ref_title">
+              <ul>
+                <li>ماژول</li>
+                <li>تعداد کردیت</li>
+              </ul>
+            </div>
+            <div className="low_ref">
+              {low_reference4.map((item) => (
+                <ul>
+                  <li>
+                    <span>{item.moudle}</span>
+                  </li>
+                  <li>
+                    <span>{item.credit}</span>
+                  </li>
+                </ul>
+              ))}
+            </div>
+          </div>
+
+          <div
+            className={
+              showTabSemester === 5
+                ? "low_tabHeader active_content box_shadow"
+                : "low_tabHeader"
+            }
+          >
+            <div className="low_ref_title">
+              <ul>
+                <li>ماژول</li>
+                <li>تعداد کردیت</li>
+              </ul>
+            </div>
+            <div className="low_ref">
+              {low_reference5.map((item) => (
+                <ul>
+                  <li>
+                    <span>{item.moudle}</span>
+                  </li>
+                  <li>
+                    <span>{item.credit}</span>
+                  </li>
+                </ul>
+              ))}
+            </div>
+          </div>
+
+          <div
+            className={
+              showTabSemester === 6
+                ? "low_tabHeader active_content box_shadow"
+                : "low_tabHeader"
+            }
+          >
+            <div className="low_ref_title">
+              <ul>
+                <li>ماژول</li>
+                <li>تعداد کردیت</li>
+              </ul>
+            </div>
+            <div className="low_ref">
+              {low_reference6.map((item) => (
+                <ul>
+                  <li>
+                    <span>{item.moudle}</span>
+                  </li>
+                  <li>
+                    <span>{item.credit}</span>
+                  </li>
+                </ul>
+              ))}
+            </div>
+          </div>
+
+          <div
+            className={
+              showTabSemester === 7
+                ? "low_tabHeader active_content box_shadow"
+                : "low_tabHeader"
+            }
+          >
+            <div className="low_ref_title">
+              <ul>
+                <li>ماژول</li>
+                <li>تعداد کردیت</li>
+              </ul>
+            </div>
+            <div className="low_ref">
+              {low_reference7.map((item) => (
+                <ul>
+                  <li>
+                    <span>{item.moudle}</span>
+                  </li>
+                  <li>
+                    <span>{item.credit}</span>
+                  </li>
+                </ul>
+              ))}
+            </div>
+          </div>
+
+          <div
+            className={
+              showTabSemester === 8
+                ? "low_tabHeader active_content box_shadow"
+                : "low_tabHeader"
+            }
+          >
+            <div className="low_ref_title">
+              <ul>
+                <li>ماژول</li>
+                <li>تعداد کردیت</li>
+              </ul>
+            </div>
+            <div className="low_ref">
+              {low_reference8.map((item) => (
+                <ul>
+                  <li>
+                    <span>{item.moudle}</span>
+                  </li>
+                  <li>
+                    <span>{item.credit}</span>
+                  </li>
+                </ul>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
 
       {/* View Point */}
       <ViewPoint
