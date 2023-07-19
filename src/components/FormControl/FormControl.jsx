dc985ee6a72d8dab4a43abe2c5603c000465bbe3
ali herawi <aliherawi7@gmail.com>
2023-07-19 23:19:13 +0430

    stepper updated

--- src/components/FormControl/FormControl.jsx
+++ src/components/FormControl/FormControl.jsx
@@ -5,13 +5,17 @@ const FormControl = ({ handleNextStep, currentStep, steps }) => {
   return (
     <div className="button_control display_flex align_items_center justify_content_space_around">
       {/* back button */}
-      <button onClick={() => handleNextStep("back")} className="back_btn btn">
-        بازگشت
-      </button>
+      {currentStep < steps.length - 1 &&
+        <button onClick={() => handleNextStep("back")} className="back_btn btn">
+          بازگشت
+        </button>
+      }
 
       {/* next button */}
       <button onClick={() => handleNextStep("next")} className="back_btn btn">
-        {currentStep === steps.length - 2 ? "تایید و ارسال" : "بعدی"}
+        {currentStep === steps.length - 2 ? "تایید و ارسال" :
+          currentStep === steps.length - 1 ? "پایان" : "بعدی"
+        }
       </button>
     </div>
   )
