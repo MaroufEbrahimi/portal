64ca7177ff23dcfe573f547d59aee7acfe9f029d
Marouf <maroufibrahimi7264@gmail.com>
2023-07-26 07:25:31 -0700

    teacher profile updated

--- src/pages/TeacherProfile/TeacherProfile.jsx
+++ src/pages/TeacherProfile/TeacherProfile.jsx
@@ -2,13 +2,31 @@ import React from "react"
 import "../Profile/Profile.css"
 import "./TeacherProfile.css"
 import { teachers } from "../../constants/Data"
+import ICONS from "../../constants/Icons"
 
 const TeacherProfile = () => {
   return (
     <div className="teacher_profile">
       {teachers.map((item, index) => (
         <div className="profile_details">
-          <div className="content_of_profile border_radius_8">
+          <div className="content_of_profile teacher_Profile_details">
+            <div className="teacher_profile_img">
+              <img src={item.teacherImg} />
+              <div class="teacher_social_media">
+                <a href="" target="_blank">
+                  <i class={ICONS.twitter}></i>
+                </a>
+                <a href={item.teacherGithub} target="_blank">
+                  <i class={ICONS.github}></i>
+                </a>
+                <a href={item.teacherLinkedin} target="_blank">
+                  <i class={ICONS.linkedin}></i>
+                </a>
+                <a href={item.teacherInstagram} target="_blank">
+                  <i class={ICONS.instagram}></i>
+                </a>
+              </div>
+            </div>
             <div className="content">
               <div className="content_boxes display_grid ">
                 <div className="content_box">
