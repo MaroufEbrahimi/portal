108676a80cda8800048acb0db41592a3fa36157a
ali herawi <aliherawi7@gmail.com>
2023-08-07 08:59:23 +0430

    print page udpated

--- src/Utils/printTableUtils.js
+++ src/Utils/printTableUtils.js
@@ -6,126 +6,169 @@ export const handlePrintTable = ({ pageTitle = 'جدول حاضری' }) => {
       <head>
       <title>${pageTitle}</title>
         <style>
-          @media print {
-            .attendance {
-              margin: 1rem 0;
-            }
 
-            .attendance .print_button {
-              text-align: left;
-            }
+          :root{
+            --gen-color: #67b0d1;
+            --gen-color-hover: #8ec4dd;
+            --gen-bg-color: #c2e2f1;
+            --gen-main-bg: #eaf8ff;
+            --text-color: #000;
+            --icon-faculties-color: #fff;
+            --btn-bg-color: #6dc7f1f3;
+            --show-color: #22222283;
+            --input-box-color: #fff;
+          }
+          * {
+            padding: 0;
+            margin: 0;
+            box-sizing: border-box;
+            font-family: vazir_l, -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto",
+              "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+              sans-serif;
+            scroll-behavior: smooth;
+            -webkit-font-smoothing: antialiased;
+            -moz-osx-font-smoothing: grayscale;
+            direction: rtl;
+          }
 
-            .attendance_faculty h2 {
-              text-align: center;
-              margin-bottom: 2rem;
-            }
 
-            .attendance_faculty_btn {
-              margin-top: 1.5rem;
-              display: flex;
-              align-items: center;
-              justify-content: center;
-            }
+          /* Attendance Content */
+          .attendance_content {
+            margin: 2rem 0 0.8rem 0;
+            border: 1px solid var(--gen-color);
+            width: 100%;
+            border-radius: 1px;
+            padding: 15px 10px;
+            overflow-x: scroll;
+          }
 
-            /* Attendance Content */
-            .attendance_content {
-              margin: 3rem 0 2rem 0;
-              border: 1px solid var(--gen-color);
-              width: 100%;
-              border-radius: 1px;
-              padding: 15px 10px;
-              overflow-x: scroll;
-            }
+          /* Attendance Header */
+          .attendance_header {
+            margin-top:40px;
+            display: grid;
+            grid-template-columns: 80% 15%;
+            gap: 5px;
+          }
 
-            /* Attendance Header */
-            .attendance_content .attendance_header {
-              display: flex;
-              align-items: center;
-              justify-content: space-between;
-              text-align: center;
-              gap: 5px;
-            }
+          .attendance_header_boxes {
+            display: grid;
+            grid-template-columns: repeat(3, 1fr);
+            text-align: center;
+            grid-gap: 5px;
+            min-width: 770px;
+            max-width: 100%;
+          }
 
-            .attendance_content .attendance_header .attendance_header_box {
-              border: 1px solid var(--gen-color);
-              width: 100%;
-            }
+          .attendance_header_boxes .attendance_header_box {
+            border: 1px solid var(--gen-color);
+            width: 100%;
+          }
 
-            .attendance_content .attendance_header .attendance_header_box p:first-child {
-              background-color: var(--gen-bg-color);
-              font-size: 15px;
-              font-weight: bold;
-            }
+          .attendance_header_boxes .attendance_header_box p {
+            font-size: 14.5px;
+            padding: 3px;
+          }
 
-            .attendance_content .attendance_header .attendance_header_box p {
-              font-size: 14.5px;
-              padding: 5px;
-            }
+          .attendance_header_boxes .attendance_header_box p:first-child {
+            background-color: var(--gen-bg-color);
+            font-size: 15px;
+            font-weight: bold;
+          }
 
-            .attendance_table {
-              margin-top: 1.7rem;
-              border: 1px solid #000;
-              border-collapse: collapse;
-              min-width: 970px;
-              max-width: 100%;
-            }
+          /* keys */
+          .attendance_header_keys {
+            min-width: 180px;
+            max-width: 100%;
+            border: 1px solid var(--gen-color);
+            border-collapse: collapse;
+          }
 
-            .holiday {
-              background-color: #ff000045;
-              border: 1px solid #ff000045;
-            }
+          .attendance_header_keys tr {
+            border: 1px solid var(--gen-color);
+            width: 100%;
+            text-align: center;
+          }
 
-            tr {
-              height: 20px;
-            }
+          .attendance_header_keys tr th {
+            border-collapse: collapse;
+            background-color: var(--gen-bg-color);
+          }
 
-            .attendance_table tr {
-              row-gap: 1px;
-            }
+          .attendance_header_keys tr td {
+            font-size: 14px;
+            font-weight: bold;
+            border: 1px solid var(--gen-color);
+          }
 
-            .attendance_table #number_counter {
-              width: 30px;
-            }
+          /* End of Attendance Header */
 
-            .attendance_table #student_name,
-            .attendance_table #student_lastname {
-              width: 100px;
-            }
+          .attendance_table {
+            margin-top: 1.7rem;
+            border: 1px solid #000;
+            border-collapse: collapse;
+            min-width: 970px;
+            max-width: 100%;
+          }
 
-            .attendance_table tr:nth-child(even) {
-              background-color: var(--gen-bg-color);
-            }
+          .holiday {
+            background-color: var(--gen-bg-color);
+            border: 1px solid var(--gen-bg-color);
+          }
 
-            .attendance_table thead tr td {
-              font-size: 12px;
-              font-weight: bold;
-            }
+          tr {
+            height: 20px;
+          }
 
-            .attendance_table tr td {
-              border: 1px solid var(--gen-color);
-              font-size: 12px;
-              padding: 2px;
-              width: 2rem;
-              text-align: center;
-            }
+          .attendance_table tr {
+            row-gap: 1px;
+          }
 
-            .attendance_table tr .data_cell {
-              text-align: center;
-              padding: 2px;
-              align-items: center;
-              justify-content: center;
-              position: relative;
-              width: 1.44rem;
-              height: 0.5rem;
-            }
+          .attendance_table #number_counter {
+            width: 30px;
+          }
 
-            .attendance_table tr .data_cell input {
-              position: absolute;
-              top: 3px;
-              left: 3px;
-              right: 3px;
-              bottom: 3px;
-            }
+          .attendance_table #student_name,
+          .attendance_table #student_lastname {
+            width: 100px;
+          }
+
+          .attendance_table tr:nth-child(even) {
+            background-color: var(--gen-bg-color);
+          }
+
+          .attendance_table thead tr td {
+            font-size: 12px;
+            font-weight: bold;
+          }
+
+          .attendance_table tr td {
+            border: 1px solid var(--gen-color);
+            font-size: 12px;
+            padding: 2px;
+            width: 2rem;
+            text-align: center;
+          }
+
+          .attendance_table tr .data_cell {
+            text-align: center;
+            padding: 2px;
+            align-items: center;
+            justify-content: center;
+            position: relative;
+            width: 1.44rem;
+            height: 0.5rem;
+          }
+
+          .attendance_table tr .data_cell input {
+            position: absolute;
+            top: 3px;
+            left: 3px;
+            right: 3px;
+            bottom: 3px;
+          }
+
+          .attendance_table tr .data_cell input[type="checkbox"] {
+            accent-color: blue;
           }
         </style>
       </head>
